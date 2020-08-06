21a506105da7adb31cb56977d13bf2cfcf91bb7c
Sergei Ponomarev <s.ponomarev@xsolla.com>
2020-08-06 07:47:50 +0500

    Fix tests

--- node/tree/__tests__/tree.test.js
+++ node/tree/__tests__/tree.test.js
@@ -26,61 +26,71 @@ const mockTree = '1\n└── 2';
 const mockTreeArr = ['1', '└── 2'];
 
 describe('Tree', () => {
-  test('generateTreeLines()', () => {
-    expect(generateTreeLines(initialParams)).toEqual(mockTreeArr);
-  });
+  describe('#generateTreeLines()', () => {
+    it('works correct', () => {
+      expect(generateTreeLines(initialParams)).toEqual(mockTreeArr);
+    });
 
-  test('generateTreeLines() with empty name', () => {
-    const newParams = {
-      ...initialParams,
-      level: {
-        ...testDataObject,
-        name: ''
+    it('works correct with empty name', () => {
+      const newParams = {
+        ...initialParams,
+        level: {
+          ...testDataObject,
+          name: ''
+        }
       }
-    }
-    expect(generateTreeLines(newParams)).toEqual([]);
+      expect(generateTreeLines(newParams)).toEqual([]);
+    });
   });
 
-  test('generateOneLine() last element on level', () => {
-    const params = {
-      levelName: '2',
-      currentDepth: 1,
-      prevSymbols: '',
-      isLastElemOnLevel: true,
-    };
-    expect(generateOneLine(params)).toEqual('└── 2');
-  });
-
-  test('generateOneLine() not last element on level', () => {
-    const params = {
-      levelName: '2',
-      currentDepth: 1,
-      prevSymbols: '',
-      isLastElemOnLevel: false,
-    };
-    expect(generateOneLine(params)).toEqual('├── 2');
+  describe('#generateOneLine()', () => {
+    it('works for last element on level', () => {
+      const params = {
+        levelName: '2',
+        currentDepth: 1,
+        prevSymbols: '',
+        isLastElemOnLevel: true,
+      };
+      expect(generateOneLine(params)).toEqual('└── 2');
+    });
+  
+    it('works for not last element on level', () => {
+      const params = {
+        levelName: '2',
+        currentDepth: 1,
+        prevSymbols: '',
+        isLastElemOnLevel: false,
+      };
+      expect(generateOneLine(params)).toEqual('├── 2');
+    });
+    
+    it('works for first level depth line', () => {
+      const params = {
+        levelName: '1',
+        currentDepth: 0,
+        prevSymbols: '',
+        isLastElemOnLevel: true,
+      };
+      expect(generateOneLine(params)).toEqual('1');
+    });
   });
   
-  test('generateOneLine() first level depth line', () => {
-    const params = {
-      levelName: '1',
-      currentDepth: 0,
-      prevSymbols: '',
-      isLastElemOnLevel: true,
-    };
-    expect(generateOneLine(params)).toEqual('1');
-  });
+  describe('#convertTreeToString()', () => {
+    it('works correct', () => {
+      const treeArr = generateTreeLines(initialParams);
+      expect(convertTreeToString(treeArr)).toEqual(mockTree);
+    });
+  })
 
-  test('convertTreeToString()', () => {
-    const treeArr = generateTreeLines(initialParams);
-    expect(convertTreeToString(treeArr)).toEqual(mockTree);
-  });
+  describe('#getNextSymbols()', () => {
+    test('works correct', () => {
+      expect(getNextSymbols(1, false)).toEqual('│   ');
+    });
+  })
 
-  test('getNextSymbols()', () => {
-    expect(getNextSymbols(1, false)).toEqual('│   ');
-  });
-
-  test('convertTreeToString()', () => {
-    expect(getLastSymbols(true)).toEqual('    ');
-  });
+  describe('#getLastSymbols()', () => {
+    test('works correct', () => {
+      expect(getLastSymbols(true)).toEqual('    ');
+    });
+  })
 });
